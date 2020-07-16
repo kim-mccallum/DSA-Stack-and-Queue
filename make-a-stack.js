2a1eab8375b7bf86e63077458a91f644af0a4c11
Kim McCallum <kchaase@gmail.com>
2020-07-15 21:29:50 -0600

    added sortStack

--- make-a-stack.js
+++ make-a-stack.js
@@ -2,6 +2,8 @@ const { Stack } = require("./stack-class");
 
 let addItems = ["Kirk", "Spock", "McCoy", "Scotty"];
 
+// let addItems = [4, 5, 3, 1, 2];
+
 let myStack = new Stack();
 
 addItems.forEach((item) => myStack.push(item));
